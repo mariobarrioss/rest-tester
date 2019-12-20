5eadce50be4ec1d1c780cef22da42b0ca47796c5
Mario Barrios <mariobarriossalazar@outlook.com>
2019-12-20 14:48:39 -0600

    Fixed linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'users/new'
   get 'users/edit'
-  post 'users/create'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users

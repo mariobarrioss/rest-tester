6a0f9b8ef364c3bc803d9badf0873a1666f93380
Mario Barrios <mariobarriossalazar@outlook.com>
2019-12-20 14:52:13 -0600

    Finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/edit'
   get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users
 end
